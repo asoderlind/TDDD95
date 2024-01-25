4595050cef4ba11335c515357d5080b71e010d21
asoderlind <axesode@gmail.com>
2024-01-25 16:37:56 +0100

    knapsack done

--- knapsack/knapsack.cpp
+++ knapsack/knapsack.cpp
@@ -1,14 +1,34 @@
+/**
+ * TDDD95: Lab 1 - knapsack
+ * Author: Axel Söderlind
+ * Date:   2024-01-25
+ * This problem is about finding the optimal set of items to
+ * put in a knapsack. We use dynamic programming to solve this
+ * problem. We create a matrix where the rows represent the
+ * items and the columns represent the capacity of the knapsack.
+ * We then populate the matrix by picking the optimal value of
+ * the previous item directly or by adding the value of the
+ * current item to the optimal value of the remaining capacity.
+ *
+ * Complexity: O(nC) where n is the number of items and C is the capacity of the knapsack.
+ * Space: O(nC) where n is the number of items and C is the capacity of the knapsack.
+ */
 #include <iostream>
 #include <vector>
 #include <algorithm>
 
 using namespace std;
 
+/// @brief Solve the knapsack problem
+/// @param C The capacity of the knapsack
+/// @param n The number of items
+/// @param values vector of values of the items
+/// @param weights vector of weights of the items
+/// @return A pair containing the optimal value and the indices of the items used
 pair<int, vector<int>> knapsack(int C, int n, const vector<int> &values, const vector<int> &weights)
 {
     vector<vector<int>> mat(n + 1, vector<int>(C + 1, 0));
 
-    // Populate the rest of the matrix
     for (int item = 1; item <= n; ++item)
     {
         for (int capacity = 1; capacity <= C; ++capacity)
