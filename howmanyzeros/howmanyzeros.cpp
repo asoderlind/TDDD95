7f4907571297445ad2c206374c43d2ce98135cf8
Axel Söderlind <axesode+github@gmail.com>
2024-01-30 16:55:41 +0100

    working howmanyzeros but I don't know why

--- howmanyzeros/howmanyzeros.cpp
+++ howmanyzeros/howmanyzeros.cpp
@@ -19,14 +19,62 @@ constexpr ll MOD = 998244353;
 constexpr ld EPS = 1e-9L;
 
 
-ll countZerosUpTo(ll n)
-{
-    return 0;
+long countZerosUpTo(long n, bool verbose = false) {
+        long zeros = 0;
+        long digit, sliceLeft, sliceRight;
+        
+        if (verbose) {
+            printf("n: %ld\n", n);
+        }
+
+        for (int i = 1; ; i *= 10) {
+
+            // Slice number and get middle digit
+            digit = n / i;
+            sliceRight = n % i;
+            sliceLeft = digit / 10;
+            digit = digit % 10;
+
+            if (verbose) {
+                printf("digit: %ld, sliceLeft: %ld, sliceRight: %ld\n", digit, sliceLeft, sliceRight);
+            }
+
+            // Whole number processed
+            if (sliceLeft == 0) {
+                if (verbose) {
+                    printf("reached end of number\n");
+                }
+                return zeros;
+            }
+
+            // Special case for digit zero
+            if (verbose) {
+                printf("zeros before: %ld\n", zeros);
+            }
+            if (digit == 0) {
+                if (verbose) {
+                    printf("zeros += %ld\n", (sliceLeft - 1) * i + sliceRight + 1);
+                }
+                zeros += (sliceLeft - 1) * i + sliceRight + 1;
+            } else {
+                if (verbose) {
+                    printf("zeros += %ld\n", sliceLeft * i);
+                }
+                zeros += sliceLeft * i;
+            }
+            if (verbose) {
+                printf("zeros after: %ld\n", zeros);
+            }
+        }
 }
 
-ll countZeros(ll n)
+long countZeros(long n)
 {
     ll count = 0;
+
+    if (n == 0)
+        return 1;
+
     while (n > 0)
     {
         if (n % 10 == 0)
@@ -41,21 +89,19 @@ int main()
     std::ios::sync_with_stdio(false);
     std::cin.tie(NULL);
     std::cout.tie(NULL);
-    ll n, m;
+    long n, m;
 
-    while (cin >> n >> m)
+    while (cin >> m >> n)
     {
-        if (n == -1 || m == -1)
+        if (m < 0)
             break;
         else {
-            ll zeros = countZeros(m);
-            ll lower = countZerosUpTo(m);
-            ll upper = countZerosUpTo(n);
+            long zeros = countZeros(m);
+            long lower = countZerosUpTo(m, true);
+            long upper = countZerosUpTo(n, true);
             cout << zeros + upper - lower << nl;
         }
     }
-
-    // read input
 
     return 0;
 }
