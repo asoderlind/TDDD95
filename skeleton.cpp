34fe8c20a36bcfe958b7a419a1fab3ae231edc17
asoderlind <axesode@gmail.com>
2024-02-19 08:15:40 +0100

    minspantree

--- skeleton.cpp
+++ skeleton.cpp
@@ -12,11 +12,17 @@ using namespace std;
 typedef long long ll;
 typedef long double ld;
 
+#define rep(i, a, b) for (int i = a; i < b; ++i)
+
+bool DEBUG = false;
+
 constexpr char nl = '\n';
 constexpr ll INF = 0x3f3f3f3f;
 constexpr ll INFLL = 0x3f3f3f3f3f3f3f3f;
 constexpr ll MOD = 998244353;
 constexpr ld EPS = 1e-9L;
+
+// ------------------------------------------- //
 
 int main()
 {
