6bddd9ea4bccebff27c5327885aa7fef736e8673
asoderlind <axesode@gmail.com>
2024-04-17 14:45:58 +0200

    finish evil straw

--- evilstraw/evilstraw.cpp
+++ evilstraw/evilstraw.cpp
@@ -3,9 +3,9 @@
  * Date:   2024-01-22
  * This problem is about ...
  */
+#include <algorithm>
 #include <iostream>
 #include <vector>
-#include <algorithm>
 
 using namespace std;
 
@@ -24,38 +24,78 @@ constexpr ld EPS = 1e-9L;
 
 // ------------------------------------------- //
 
-int getNumSwaps(string s)
-{
-    int numSwaps = 0;
-    int n = s.size();
-    for (int i = 0; i < n / 2; ++i)
-    {
-        if (s[i] != s[n - i - 1])
-        {
-            numSwaps++;
-        }
+bool isShuffledPalindrome(const string& s) {
+  vector<int> occurrence(26, 0);
+  int odd_count = 0;
+  for (char i : s) {
+    occurrence[i - 'a']++;
+  }
+  for (int value : occurrence) {
+    if (value % 2 != 0) {
+      odd_count++;
     }
-    return numSwaps;
+  }
+  return odd_count <= 1;
 }
 
-int main()
-{
-    std::ios::sync_with_stdio(false);
-    std::cin.tie(NULL);
-    std::cout.tie(NULL);
+int getNumSwaps(string s) {
+  int stringLen = s.size();
+  int result = 0;
+  int start = 0, end = stringLen - 1;
 
-    // read input
-    int numCases;
-    cin >> numCases;
+  // if string is empty or it is not a palindrome return -1
+  if ((stringLen == 0) || (!isShuffledPalindrome(s))) {
+    return -1;
+  }
 
-    for (int i = 0; i < numCases; ++i)
-    {
-        // read input
-        string s;
-        cin >> s;
-        int numSwaps = getNumSwaps(s);
-        cout << s << ": " << numSwaps << nl;
+  while (end > start) {
+    if (s[start] != s[end]) {
+      int i = end;  // make an additional iterator from the end
+
+      while (i > start && s[i] != s[start]) {
+        --i;
+      }
+
+      if (i == start) {
+        swap(s[start], s[start + 1]);
+        ++result;
+      } else {
+        while (i < end) {
+          swap(s[i], s[i + 1]);
+          ++result;
+          ++i;
+        }
+        ++start;
+        --end;
+      }
+    } else {
+      ++start;
+      --end;
     }
+  }
+  return result;
+}
 
-    return 0;
+int main() {
+  std::ios::sync_with_stdio(false);
+  std::cin.tie(NULL);
+  std::cout.tie(NULL);
+
+  // read input
+  int numCases;
+  cin >> numCases;
+
+  for (int i = 0; i < numCases; ++i) {
+    // read input
+    string s;
+    cin >> s;
+    int numSwaps = getNumSwaps(s);
+    if (numSwaps == -1) {
+      cout << "Impossible" << nl;
+    } else {
+      cout << numSwaps << nl;
+    }
+  }
+
+  return 0;
 }
